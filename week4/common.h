9a03a36e8f5e001e29a95dd639950ce94b3babce
BoogiemanNSK <calah47@yandex.ru>
2019-02-15 12:52:32 +0300

    Week 4 N Lab

---
+++ week4/common.h
@@ -0,0 +1,12 @@
+typedef struct _test_struct{
+    char s_name[32];
+    unsigned int s_age;
+    char s_group_name[32];
+} test_struct_t;
+
+
+typedef struct result_struct_{
+
+    char s_info[128];
+
+} result_struct_t;
