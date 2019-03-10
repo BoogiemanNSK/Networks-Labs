555c810ac5da7c9c10e09f26cd75f5f638fd0aec
BoogiemanNSK <calah47@yandex.ru>
2019-03-10 19:08:23 +0300

    Week 8 OS Lab almost there

---
+++ week6/alist.h
@@ -0,0 +1,30 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <beluckydaf@gmail.com> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Vladislav Smirnov
+ * ----------------------------------------------------------------------------
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct {
+    size_t size;
+    size_t count;
+    void** array;
+} array_list;
+
+typedef array_list* p_array_list;
+
+p_array_list create_array_list(size_t size);
+void delete_array_list(p_array_list alist);
+size_t expand_array_list(p_array_list alist);
+int array_list_add(p_array_list alist, void* item);
+int array_list_remove(p_array_list alist, void* item);
+int array_list_remove_at(p_array_list alist, int index);
+int array_list_iter(p_array_list alist);
+int array_list_next(p_array_list alist, int index);
+void* array_list_get(p_array_list alist, int index);
--- week6/node.c
+++ week6/node.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
@@ -8,86 +9,324 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
-#define SERVER_PORT     2000
+#include "alist.h"
 
-char data_buffer[1024];
+#define NETWORK_IP_ADDRESS "192.168.0.0"
+#define NETWORK_PORT 2000
 
-struct message_struct {
-    char data[256];
-};
+#define LIST_REQUEST "GET_LIST\n"
+#define PING "PING"
+#define PONG "PONG"
 
-void setup_tcp_server_communication() {
-    int master_sock_tcp_fd = 0,
-            sent_recv_bytes = 0,
-            addr_len = 0,
-            opt = 1;
+#define PING_SIZE 4
+#define MAX_LIST_SIZE 1024
 
-    fd_set readfds;
-    int comm_socket_fd = 0;
+p_array_list nodes;
+
+typedef struct node {
+    char[32] name;
+    char[16] ip;
+    char[16] port;  
+} node;
+
+void udp_receive_ping() {
+    char ping_buffer[PING_SIZE];
+    int ping_recv_socket = 0, addr_len = 0;
     struct sockaddr_in server_addr, client_addr;
 
-    if ((master_sock_tcp_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
-        printf("Master socket creation failed.\n");
-        exit(1);
-    }
+    ping_recv_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = SERVER_PORT;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
     addr_len = sizeof(struct sockaddr);
+    memset(&server_addr, 0, addr_len);
+    
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(master_sock_tcp_fd, (struct sockaddr *) &server_addr, sizeof(struct sockaddr)) == -1) {
-        printf("Master socket bind failed\n");
-        return;
-    }
-
-    struct sockaddr_in sin;
-    socklen_t len = sizeof(sin);
-    if (getsockname(master_sock_tcp_fd, (struct sockaddr *)&sin, &len) == -1)
-        perror("getsockname");
-    else
-        printf("port number %d\n", ntohs(sin.sin_port));
-
-    if (listen(master_sock_tcp_fd, 5) < 0) {
-        printf("Master socket listen failed\n");
-        return;
-    }
+    bind(ping_recv_socket, (struct sockaddr *) &server_addr, addr_len);
 
     while (1) {
-        FD_ZERO(&readfds);
-        FD_SET(master_sock_tcp_fd, &readfds);
+        memset(ping_buffer, 0, PING_SIZE);
+        memset(&client_addr, 0, addr_len);
 
-        select(master_sock_tcp_fd + 1, &readfds, NULL, NULL, NULL);
+        recvfrom(ping_recv_socket, (void *)ping_buffer, PING_SIZE, MSG_WAITALL,
+            (struct sockaddr *) &client_addr, &addr_len);
 
-        if (FD_ISSET(master_sock_tcp_fd, &readfds)) {
+        if (strncmp(PING, ping_buffer, PING_SIZE) == 0) {
+            sendto(ping_recv_socket, PONG, PING_SIZE, MSG_CONFIRM,
+		        (struct sockaddr *) client_addr, addr_len);
+        }
+    }
+}
 
-            comm_socket_fd = accept(master_sock_tcp_fd, (struct sockaddr *) &client_addr, &addr_len);
-            if (comm_socket_fd < 0) {
-                printf("accept error : errno = %d\n", errno);
-                exit(0);
-            }
+void udp_send_ping() {
+    char ping_buffer[PING_SIZE];
+    int sockfd = 0, addr_len = 0;
+    struct sockaddr_in dest;
+    struct hostent *host;
 
-            printf("Connection accepted from client : %s:%u\n",
-                   inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    addr_len = sizeof(struct sockaddr);
 
-            while (1) {
-                memset(data_buffer, 0, sizeof(data_buffer));
+    sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-                sent_recv_bytes = recvfrom(comm_socket_fd, (char *) data_buffer, sizeof(data_buffer), 0,
-                                           (struct sockaddr *) &client_addr, &addr_len);
+    while(1) {
+        node* current;
+        int i = array_list_iter(nodes);
+        
+        for (; i != -1; i = array_list_next(nodes, i)) {
+            current = array_list_get(nodes, i);
 
-                // TODO Compose handshake message.
-                message_struct message;
+            host = (struct hostent *)gethostbyname(current->ip);
+            dest.sin_family = AF_INET;
+            dest.sin_port = htons(atoi(current->ip));
+            dest.sin_addr = *((struct in_addr *)host->h_addr);
 
-                sent_recv_bytes = sendto(comm_socket_fd, (char *) &message, sizeof(message_struct), 0,
-                                         (struct sockaddr *) &client_addr, sizeof(struct sockaddr));
+	        printf("PING %s:%s:%u", current->name, inet_ntoa(dest.sin_addr), ntohs(dest.sin_port));
+
+	        sendto(sockfd, PING, PING_SIZE, MSG_CONFIRM,
+		        (struct sockaddr *)&dest, addr_len);
+
+            printf(" - PING SENT - ");
+
+	        recvfrom(sockfd, ping_buffer, PING_SIZE, MSG_WAITALL,
+                (struct sockaddr *)&dest, &addr_len);
+
+            if (strncmp(PONG, ping_buffer, PING_SIZE) == 0) {
+                printf("PONG RECEIVED\n");
             }
         }
     }
 }
 
+void tcp_request_network_connection() {
+    int sock_fd = 0, addr_len = 0;
+    struct sockaddr_in dest;
+    struct hostent *host;
+    char name[32];
+    char message[48];
+
+    printf("Your Network name: ");
+    scanf("%s", name);
+
+    int i = 0, j = 0;
+    for (; i < strlen(LIST_REQUEST); i++) {
+        message[i] = LIST_REQUEST[i];
+    }
+    for (; j < strlen(name); i++, j++) {
+        message[i] = name[j];
+    }
+    message[i] = "\n.\n";
+    
+    addr_len = sizeof(struct sockaddr);
+    
+    host = (struct hostent *)gethostbyname(NETWORK_IP_ADDRESS);
+    dest.sin_family = AF_INET;
+    dest.sin_port = NETWORK_PORT;
+    dest.sin_addr = *((struct in_addr *)host->h_addr);
+
+    sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    connect(sock_fd, (struct sockaddr *)&dest, addr_len);
+
+	sendto(sock_fd, message, sizeof(message), 0,
+		    (struct sockaddr *)&dest, addr_len);
+    
+    char* received_data = (char*)malloc(MAX_LIST_SIZE);
+
+	recvfrom(sock_fd, (char *)&received_data, MAX_LIST_SIZE, 0,
+		    (struct sockaddr *)&dest, &addr_len);
+
+    int start = 0, end = 0;
+    for (; end < MAX_LIST_SIZE; end++) {
+        if (received_data[end] == '\n') {
+            node* new_node = (node*)malloc(sizeof(node));
+            
+            int i = 0;
+            while (received_data[start] != ':') {
+                new_node->name[i++] = received_data[start++];
+            }
+            i = 0;
+            start++;
+            while (received_data[start] != ':') {
+                new_node->ip[i++] = received_data[start++];
+            }
+            i = 0;
+            start++;
+            while (start != end) {
+                new_node->port[i++] = received_data[start++];
+            }
+
+            array_list_add(nodes, new_node);
+            start = end + 1;
+        }
+        if (strncmp(received_data + end, "\n.\n", 3) == 0) {
+            break;
+        }
+    }
+
+    free(receieved_data);
+    close(sock_fd);
+}
+
+void tcp_listen() {
+    char data_buffer[1024];
+    int master_sock_fd = 0, comm_sock_fd = 0, addr_len = 0;
+    struct sockaddr_in server_addr, client_addr;
+    fd_set readfds;
+
+    addr_len = sizeof(struct sockaddr);
+
+    master_sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = SERVER_PORT;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    
+    bind(master_sock_fd, (struct sockaddr *) &server_addr, addr_len);
+
+    listen(master_sock_fd, 5);
+
+    while (1) {
+        memset(data_buffer, 0, sizeof(data_biffer));
+        FD_ZERO(&readfds);
+        FD_SET(master_sock_fd, &readfds);
+
+        select(master_sock_fd + 1, &readfds, NULL, NULL, NULL);
+
+        if (FD_ISSET(master_sock_fd, &readfds)) {
+            comm_sock_fd = accept(master_sock_fd, (struct sockaddr *) &client_addr, &addr_len);
+            
+            while (1) {
+                recvfrom(comm_sock_fd, (char *) data_buffer, sizeof(data_buffer), 0,
+                    (struct sockaddr *) &client_addr, &addr_len);
+
+                if (strncmp(data_buffer, LIST_REQUEST, strlen(LIST_REQUEST)) == 0) {
+                    char name[32];
+                    int i = strlen(LIST_REQUEST) + 1, j = 0, k;
+                    while (data_buffer[i] != '\n') {
+                        name[j++] = data_buffer[i++];
+                    }
+
+                    // Send list
+                    node* current;
+                    i = array_list_iter(nodes), j = 0;
+                    memset(data_buffer, 0, sizeof(data_biffer));
+        
+                    for (; i != -1; i = array_list_next(nodes, i)) {
+                        current = array_list_get(nodes, i);
+                        
+                        for (k = 0; current->name[k] != '\0'; j++, k++) {
+		                    data_buffer[j] =  current->name[k];
+	                    }
+                        data_buffer[j++] = ':';
+                        for (k = 0; current->ip[k] != '\0'; j++, k++) {
+		                    data_buffer[j] =  current->ip[k];
+	                    }
+                        data_buffer[j++] = ':';
+                        for (k = 0; current->port[k] != '\0'; j++, k++) {
+		                    data_buffer[j] =  current->port[k];
+	                    }
+                        data_buffer[j++] = '\n';
+                    }
+
+                    data_buffer[j++] = '.';
+                    data_buffer[j] = '\n';
+                    
+                    sendto(comm_sock_fd, data_buffer, sizeof(data_buffer), 0,
+                        (struct sockaddr *) &client_addr, addr_len);
+
+                    node* new_node = (node*)malloc(sizeof(node));
+                    new_node->name = name;
+                    new_node->ip = inet_ntoa(client_addr->sin_addr);
+                    new_node->port = ntohs(client_addr->sin_port);
+
+                    new_node_info(new_node);
+                    array_list_add(nodes, new_node);
+                } else {
+                    // Add new node
+                    int start = 0, end = 0, i = 0;
+                    while (data_buffer[end] != '\n') { end++; }
+
+                    node* new_node = (node*)malloc(sizeof(node));
+                    while (data_buffer[start] != ':') {
+                        new_node->name[i++] = receidata_buffered_data[start++];
+                    }
+                    i = 0;
+                    start++;
+                    while (data_buffer[start] != ':') {
+                        new_node->ip[i++] = data_buffer[start++];
+                    }
+                    i = 0;
+                    start++;
+                    while (start != end) {
+                        new_node->port[i++] = data_buffer[start++];
+                    }
+
+                    array_list_add(nodes, new_node);
+                }
+            }
+        }
+    }
+}
+
+void new_node_info(node* new_node) {
+    int sock_fd = 0, addr_len = 0;
+    struct sockaddr_in dest;
+    struct hostent *host;
+    char message[48];
+    node* current;
+
+    int i, k, j = 0;
+    for (k = 0; new_node->name[k] != '\0'; j++, k++) {
+		message[j] = new_node->name[k];
+	}
+    message[j++] = ':';
+    for (k = 0; new_node->ip[k] != '\0'; j++, k++) {
+		message[j] = new_node->ip[k];
+	}
+    message[j++] = ':';
+    for (k = 0; new_node->port[k] != '\0'; j++, k++) {
+		message[j] = new_node->port[k];
+	}
+    message[j] = "\n.\n";
+    
+    addr_len = sizeof(struct sockaddr);
+    
+    sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    i = array_list_iter(nodes), j = 0;        
+    for (; i != -1; i = array_list_next(nodes, i)) {
+        current = array_list_get(nodes, i);
+        
+        host = (struct hostent *)gethostbyname(current->ip);
+        dest.sin_family = AF_INET;
+        dest.sin_port = htons(atoi(current->ip));
+        dest.sin_addr = *((struct in_addr *)host->h_addr);
+
+        connect(sock_fd, (struct sockaddr *)&dest, addr_len);
+
+	    sendto(sock_fd, message, sizeof(message), 0,
+            (struct sockaddr *)&dest, addr_len);
+
+        close(sock_fd);
+    }
+}
+
 int main(int argc, char **argv) {
-    setup_tcp_server_communication();
+    nodes = create_array_list(16);
+    pthread_t udp_send_ping_thread;
+    pthread_t udp_receive_ping_thread;
+
+    // Ask for list of connected nodes
+    tcp_request_network_connection();
+
+    // Create PING/PONG loops
+    pthread_create(&udp_send_ping_thread, NULL, udp_send_ping, NULL);
+    pthread_create(&udp_receive_ping_thread, NULL, udp_receive_ping, NULL);
+
+    // Listen for new nodes info
+    tcp_listen();
     return 0;
 }
